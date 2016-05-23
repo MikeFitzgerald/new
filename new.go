e20395309e7fa15eee7f40ededd259d6c9530d3e
Mike Fitzgerald <mikeyfitzy@gmail.com>
2016-05-23 01:21:23 -0400

    Init.

---
+++ new.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	args := os.Args[1:]
+
+	switch args[0] {
+	case "project":
+		project(args[1])
+	default:
+		fmt.Println("I'm not sure what you want to do.")
+	}
+
+}
+
+// Create a new programming project
+func project(name string) {
+	data := []byte("# " + properTitle(name))
+	//empty := []byte("")
+	files := []string{".gitattributes", ".gitignore"}
+
+	// Create a new directory with the project's name
+	// Create a README.md
+	// Create a .gitattributes file
+	// Create a .gitignore file
+	err := os.Mkdir(name, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(name+"/README.md", data, 0644)
+	check(err)
+
+	for _, file := range files {
+		err := ioutil.WriteFile(name+"/"+file, []byte(""), 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func properTitle(input string) string {
+	words := strings.Fields(input)
+	smallwords := "a an on the to"
+	for index, word := range words {
+		if strings.Contains(smallwords, " "+word+" ") {
+			words[index] = word
+		} else {
+			words[index] = strings.Title(word)
+		}
+	}
+	return strings.Join(words, " ")
+}
